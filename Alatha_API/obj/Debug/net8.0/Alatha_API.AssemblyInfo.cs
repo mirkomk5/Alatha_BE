134caabdc821102381770de635d4fd0f24bc04b2
MirkoMatt <mirko.mataluni@ariestech.it>
2025-06-25 19:33:06 +0200

    scaffold new database from freehost

--- Alatha_API/obj/Debug/net8.0/Alatha_API.AssemblyInfo.cs
+++ Alatha_API/obj/Debug/net8.0/Alatha_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alatha_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae30e5534df6817aec6ead4a4eede6491b35126b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39c80a2c90fb96c0a8b249d0108566eb3f879f7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alatha_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alatha_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
